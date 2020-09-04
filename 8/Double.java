b61a81af31dcc30dd964b22a512d0de591223198
teakyung25 <taekyung.kim25@gmail.com>
2020-09-04 07:38:40 -0600

    Finished exercise 8.2

---
+++ 8/Double.java
@@ -0,0 +1,6 @@
+public class Double {
+    public static void main ( String[] args ) {
+      double value = 3222222222222222222222222222222;
+      System.out.println("A double: " +  value);
+    }
+}
