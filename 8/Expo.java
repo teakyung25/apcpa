76f621f91687c069d2178d05aa47580a2212eceb
teakyung25 <taekyung.kim25@gmail.com>
2020-09-04 07:39:04 -0600

    Finished exercise 8.3

---
+++ 8/Expo.java
@@ -0,0 +1,6 @@
+public class Expo {
+    public static void main (String [] args){
+        double value = 1064;
+        System.out.println("e to the power value: " +  Math.exp( value ) );
+    }
+}
