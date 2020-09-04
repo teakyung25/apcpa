d8e9615843ed19c03675878e06ae7a34e4e8fa1b
teakyung25 <taekyung.kim25@gmail.com>
2020-09-04 07:49:11 -0600

    Finished exercise 8.4

---
+++ 8/Assassination.java
@@ -0,0 +1,9 @@
+public class Assassination {
+  public static void main ( String[] args )
+  {
+    char ch = "A" ;
+    System.out.println("A char: " +  ch );
+  }
+}
+
+//Double quotes or more than one char represent strings and there must a be a char between the single quotes 
