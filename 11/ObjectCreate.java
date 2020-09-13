4c6a25eb1c8d47ad36bbdb85888dbafd511a5676
teakyung25 <taekyung.kim25@gmail.com>
2020-09-13 17:02:51 -0600

    Completed 11.2

---
+++ 11/ObjectCreate.java
@@ -0,0 +1,8 @@
+public class ObjectCreate {
+    public static void main (String [] args ){
+        String str = "Hello";
+        System.out.println(str);
+        String secondVariable;
+        System.out.println(secondVariable);
+    }
+}
